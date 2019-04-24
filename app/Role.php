b9e905fc0509df25086f0a66e1dc883ddc719010
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-04-24 02:54:48 -0300

    migracion tabla permissions, controlador resource y modelo, trabajo para modelos para roles y permisos

--- app/Role.php
+++ app/Role.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     //
+    protected $filiable=['name','slug','description'];
+
+    //RELACIONES
+
+    public function permissions(){
+        return $this->hasMany(Permission::class);
+    }
+
+    public function users(){
+        return $this->belongsToMany(User::class);
+    }
+
+//ALMACENAMIENTO
+
+//VALIDACION
+
+//RECUPERACION DE INFORMACION
+
+//OTRAS OPERACIONES
 }
+
