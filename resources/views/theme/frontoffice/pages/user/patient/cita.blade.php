34d12291143ddd490abeb63edf48ea6bc5eca378
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-05-07 01:58:44 -0300

    implementacion libreria jquery pickadate

--- resources/views/theme/frontoffice/pages/user/patient/cita.blade.php
+++ resources/views/theme/frontoffice/pages/user/patient/cita.blade.php
@@ -4,6 +4,9 @@
 @endsection
 
 @section('head')
+   <link rel="stylesheet" href="{{asset('assets/frontoffice/js/plugins/pickadate/themes/default.css')}}">
+   <link rel="stylesheet" href="{{asset('assets/frontoffice/js/plugins/pickadate/themes/default.date.css')}}">
+   <link rel="stylesheet" href="{{asset('assets/frontoffice/js/plugins/pickadate/themes/default.time.css')}}">
 
 @endsection
 
@@ -20,6 +23,12 @@
                 <div class="card">
                     <div class="card-content">
                         <span class="card-title">@yield('title')</span>
+                        <form method="post" action="#">
+                            <input type="text" name="doctor">
+                            <input type="text" name="date" class="datepicker">
+                            <input type="text" name="time" class="timepicker">
+
+                        </form>
 
 
                     </div>
@@ -35,6 +44,18 @@
 @endsection
 
 @section('foot')
+<script src="{{asset('assets/frontoffice/js/plugins/pickadate/picker.js')}}"></script>
+<script src="{{asset('assets/frontoffice/js/plugins/pickadate/picker.date.js')}}"></script>
+<script src="{{asset('assets/frontoffice/js/plugins/pickadate/picker.time.js')}}"></script>
+<script src="{{asset('assets/frontoffice/js/plugins/pickadate/legacy.js')}}"></script>
+<script>
+    $('.datepicker').pickadate({
+
+    });
+    $('.timepicker').pickatime({
+
+    });
+</script>
 
 
 @endsection
