b2c337d1af813d25942bc3952fa57707088ea9b6
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-05-11 01:24:39 -0300

    arreglando navegacion(agregar active segun ruta) en el perfil para agregar rol/permisos al usuario

--- resources/views/theme/backoffice/pages/user/includes/user_nav.blade.php
+++ resources/views/theme/backoffice/pages/user/includes/user_nav.blade.php
@@ -1,8 +1,8 @@
 <div class="collection">
 
     {{-- <a href="#!" class="collection-item">Alvin</a> --}}
-    <a href="{{route('backoffice.user.show',$user)}}" class="collection-item active">{{$user->name}}</a>
-    <a href="{{route('backoffice.user.assign_role',$user)}}" class="collection-item">Asignar roles</a>
-    <a href="{{route('backoffice.user.assign_permission',$user)}}" class="collection-item">Asignar permisos</a>
+    <a href="{{route('backoffice.user.show',$user)}}" class="collection-item {{active_class(route('backoffice.user.show',$user))}}">{{$user->name}}</a>
+    <a href="{{route('backoffice.user.assign_role',$user)}}" class="collection-item {{active_class(route('backoffice.user.assign_role',$user))}}">Asignar roles</a>
+    <a href="{{route('backoffice.user.assign_permission',$user)}}" class="collection-item {{active_class(route('backoffice.user.assign_permission',$user))}}">Asignar permisos</a>
 
 </div>
